531398ab18a929f54deb516a0adfb1fb0689be4b
Xyrephon <jtuc1613@740s-pl-115.local>
2016-12-01 08:31:42 -0700

    Started Pokemon Project.

---
+++ src/pokemon/controller/PokemonController.java
@@ -0,0 +1,15 @@
+package pokemon.controller;
+
+public class PokemonController
+{
+
+	public PokemonController()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+}
---
+++ src/pokemon/controller/PokemonRunner.java
@@ -0,0 +1,10 @@
+package pokemon.controller;
+
+public class PokemonRunner
+{
+	public static void main (String [] args)
+	{
+		PokemonController PokeAppController = new PokemonController();
+		PokeAppController.start();
+	}
+}
